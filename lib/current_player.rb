1e182347410071c21dda974a566f4ae015e8e4a4
pchengeducation@hotmail.com <pchengeducation@hotmail.com>
2018-06-23 00:32:06 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,12 +2,12 @@ def turn_count(board)
 
 end
 
-def current_player(board)
+#def current_player(board)
 
-if turn_count(board)%2
-return "X"
+#if turn_count(board)%2
+#return "X"
 
-elsif !turn_count(board)%2
-return "O"
+#elsif turn_count(board)%2
+#return "O"
 
-end
+#end
